1bda8c2ac0c9a90f32f7da87e75951c75730dda8
Garrett Winter <Garrett_Winter@hotmail.com>
2022-11-13 16:37:44 -0600

    linked the highscore page to the script file. did a pass at psedo coding for functions.

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,10 +24,32 @@ function startTimer() {
 /* Psedo Coding
 Functions:
     Timer
+        add GameOver Function when timer hits 0
     Play Game
-    Win Game
-    Lose Game
+        Add time to clock
+        Copy question list to a un-asked variable
+        Random Select a question
+            will need to do a math calculation to ran select
+            Move questions from un-asked to asked to prevent being repeated
+            Right / Wrong logic
+                if right
+                    grant points 10 points
+                Diplay a right or wrong message
+            Loop to next question 
+    GameOver
+        Show user final score
+        Diplay message "Great job"
+        ask for users name
+        user submits score
+            writes to local stoage using a JSON stringify
+            Redirects user to high score page
+        
+ 
     Record Keeping (high Score Page)
+        reads local store and displays using a JSON Parse
 
-
+Extras if have extra time
+    award extra points if questions awsered fast
+        will require a second timer for the question
+    
 */
